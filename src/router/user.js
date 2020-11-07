67e7ff22c98fef2efc9e3c08233f0042a21ee184
abhirahul96 <abhi.rahulnayak@gmail.com>
2020-11-07 16:44:44 +0530

    user router

---
+++ src/router/user.js
@@ -0,0 +1,165 @@
+const express = require('express')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const router = new express.Router()
+
+
+//Create user in the data base or signup
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
+
+    // user.save().then(() => {
+    //     res.status(201).send(user)
+    // }).catch((error) => {
+    //     res.status(400).send(error)
+    // })
+})
+
+//User login
+router.post('/users/login', async (req, res) => {
+
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+//Logout user by deleting token from the token array
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+
+//Logout from all devices by deleting all token from the token array
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+
+//Get all the users from the database
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+
+
+
+    // try {
+    //     const users = await User.find({})
+    //     res.send(users)
+    // } catch (e) {
+    //     res.status(500).send(e)
+    // }
+
+    // User.find({}).then((users) => {
+    //     res.send(users)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
+})
+
+
+// //Get the users matching to the id
+// router.get('/users/:id', async (req, res) => {
+//     const _id = req.params.id
+
+//     try {
+
+//         const user = await User.findById(_id)
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(user)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+
+
+//     // User.findById(_id).then((user) => {
+//     // if (!user) {
+//     //     return res.status(404).send()
+//     // }
+//     //     res.send(user)
+//     // }).catch((e) => {
+//     //     res.statuss(404).send()
+//     // })
+// })
+
+//Update an existing user data
+router.patch('/users/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try {
+        // const user = await User.findById(req.params.id)
+
+        // updates.forEach((update) => user[update] = req.body[update])
+        updates.forEach((update) => req.user[update] = req.body[update])
+
+        await req.user.save()
+
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        // if (!user) {
+        //     return res.status(404).send()
+        // }
+
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//Delete a user
+router.delete('/users/me', auth, async (req, res) => {
+    try {
+        // const user = await User.findByIdAndDelete(req.user._id)
+
+        // if (!user) {
+        //     return res.status(404).send()
+        // }
+
+        await req.user.remove()
+        res.send(req.user)
+
+        res.send(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+
+module.exports = router
