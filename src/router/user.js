107856b718b93d604472f99415175ccc5438d87b
abhirahul96 <abhi.rahulnayak@gmail.com>
2020-11-11 23:32:20 +0530

    image upload

--- src/router/user.js
+++ src/router/user.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const multer = require('multer')
+
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+
 
 
 //Create user in the data base or signup
@@ -161,5 +164,23 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image file'))
+        }
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
 
 module.exports = router
